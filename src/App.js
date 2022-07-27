6bbe7df2a14102621d1825066fdd84ff7e64fbb3
minhphuongk57.dev <minhphuongk57.dev@gmail.com>
2022-07-28 05:15:00 +0700

    Update resourceS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import ListSongs from "./components/ListSongs";
+import Navbar from "./components/Navbar";
+import PlayingSong from "./components/PlayingSong";
+import SongDetail from "./components/SongDetail";
+import { Songs } from "./Context";
+import DataSongs from "./data/listsongs.json";
 function App() {
+  const [song, setSong] = useState(DataSongs[0]);
+
+  const handleSetSong = (idSong) => {
+    const song = DataSongs.find((song) => song.id === idSong);
+    if (!song) setSong(DataSongs[0]);
+    else setSong(song);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Songs.Provider value={{ DataSongs, song, handleSetSong }}>
+        <Navbar />
+        <div className="grid grid-cols-3 bg-slate-700 h-screen-navbar-player overflow-hidden">
+          <SongDetail />
+          <ListSongs />
+        </div>
+        <PlayingSong />
+      </Songs.Provider>
     </div>
   );
 }
